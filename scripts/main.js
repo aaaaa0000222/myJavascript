73e57774fae3b8e5411289ef1fb1281e46e65bc2
Ubuntu9x <thjkjnoidi@gmail.com>
2020-05-15 23:30:40 +0700

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+myButton.onclick = function () {
+  // body...
+  setUserName();
+}
